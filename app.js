8cd65b01f2853ea5ab69e8b85aeb805f5ec033da
Claire Lovisa <lovisa.claire@gmail.com>
2022-05-13 17:07:29 +0200

    Refactor code to do sharepoint calls

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 import { app, errorHandler } from 'mu';
 import bodyParser from 'body-parser';
 import flatten from 'lodash.flatten';
-import { getQueryParams } from './lib/delta-processing';
-import {
-  USERNAME,
-  PASSWORD,
-  SITE,
-  LIST
-} from './config';
-
-const $SP = require("sharepointplus/dist");
+import { updateSharepointList } from './lib/sharepoint-helpers';
 
 // TODO - Log an error + send an email each time a syncing fails
 // See https://github.com/lblod/delta-consumer-file-sync-submissions/blob/master/lib/error.js
@@ -46,45 +38,5 @@ app.post('/delta', async function (req, res) {
 
   res.status(202).send();
 });
-
-/**
- * Update the sharepoint list linked to some deltas.
- * The order is important : we should first process the deletes and only once it's done the inserts.
- *
- * @param {Array} deltas The deltas from which to deduce the query params
- */
-async function updateSharepointList(deletes, inserts) {
-  try {
-    const deletesQueryParams = await getQueryParams(deletes);
-    const insertsQueryParams = await getQueryParams(inserts);
-
-    console.log('deletesQueryParams', deletesQueryParams);
-    console.log('insertsQueryParams', insertsQueryParams);
-
-    const credentialOptions = {
-      username: USERNAME,
-      password: PASSWORD
-    };
-
-    const sp = $SP().auth(credentialOptions);
-
-    // Example of GET
-    const res = await sp.list(LIST, SITE).get({
-      fields: "Title,KBOnr"
-    });
-      
-    res.map(data => {
-      console.log(data.getAttribute('Title'), ' and ', data.getAttribute('KBOnr'));
-    });
-
-    // Example of UPDATE
-    sp.list(LIST, SITE).update(
-      { KBOnr: "TEST" }, // 0751541350
-      { where: "Title = 'Digipolis_Antwerpen'" }
-    );
-  } catch (e) {
-    console.log('Error: ', e);
-  }
-}
 
 app.use(errorHandler);
--- config.js
+++ config.js
@@ -17,3 +17,7 @@ export const LIST = process.env.LIST;
 if(!process.env.SITE)
   throw `Expected 'SITE' to be provided.`;
 export const SITE = process.env.SITE;
+
+if(!process.env.SHAREPOINT_UUID_FIELD_NAME)
+  throw `Expected 'SITE' to be provided.`;
+export const SHAREPOINT_UUID_FIELD_NAME = process.env.SHAREPOINT_UUID_FIELD_NAME;
